0b46e0c24a308b68a5c7183b2a408a9d1c9b4a29
Gekif <dzulfikar.maulana@gmail.com>
2019-08-12 12:46:57 +0700

    4. Apache Kafka Hands On Practice: Writing your own producer

---
+++ src/main/java/KafkaProducer.java
@@ -0,0 +1,55 @@
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class KafkaProducer {
+
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+
+        // Kafka bootstrap server
+        properties.setProperty("bootstrap.servers", "192.168.99.100:9092");
+        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("value.serializer", StringSerializer.class.getName());
+
+
+        // Producer acks
+        properties.setProperty("acks", "1");
+        properties.setProperty("retries", "3");
+        properties.setProperty("linger.ms", "1");
+
+
+        // Launch Kafka Producer
+        Producer<String, String> producer = new org.apache.kafka.clients.producer.KafkaProducer<String, String>(properties);
+
+
+
+
+        // Create producer records
+/*        ProducerRecord<String, String> producerRecord = new ProducerRecord<String, String>
+//                ("second_topic", "3", "message test");
+                ("second_topic", "3", "another test");*/
+
+
+        // Using For Loop To Create Producer Records
+        for (int key = 0; key < 10; key++) {
+            ProducerRecord<String, String> producerRecord = new ProducerRecord<String, String>
+                    ("second_topic", Integer.toString(key), "message that has key: " + Integer.toString(key));
+
+            producer.send(producerRecord);
+        }
+
+
+        // Send The Data
+//        producer.send(producerRecord);
+
+
+
+        // Close The Producer
+        producer.close();
+
+
+    }
+}
